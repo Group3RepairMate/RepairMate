c750f214cdd70d38b70028bdf8db16a9bcf6deb1
chintan <patelchintan1851@gmail.com>
2023-06-06 13:14:53 -0400

    Initial Commit

---
+++ RepairmateHome/RepairmateHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepairmateHomeApp.swift
+//  RepairmateHome
+//
+//  Created by Patel Chintan on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct RepairmateHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
