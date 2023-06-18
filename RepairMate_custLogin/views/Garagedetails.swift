96563d8d88c828ff7d2128f6feecc1859662a3a4
harshil3662 <vaghaniharshil11291@gmail.com>
2023-06-18 01:27:45 -0400

    Map Activity

--- RepairMate_custLogin/views/Garagedetails.swift
+++ RepairMate_custLogin/views/Garagedetails.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 import CoreLocation
 import _MapKit_SwiftUI
+import MapKit
+
+struct Place: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct Garagedetails: View {
     var detailsview:Garage
+    @State private var place : [Place] = []
     @State private var name : String = ""
     @State private var location : String = ""
     @State private var email : String = ""
@@ -23,9 +30,10 @@ struct Garagedetails: View {
     var body: some View {
         VStack{
             Text(detailsview.name)
-//                .font(.title)
+            //                .font(.title)
                 .foregroundColor(Color("darkgray"))
                 .font(.system(size: 30))
+                .fontWeight(.semibold)
             Text("")
             VStack{
                 
@@ -34,9 +42,30 @@ struct Garagedetails: View {
                     .font(.system(size: 17))
                     .foregroundColor(.gray)
                 Text("")
-                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
-                    .frame(width: 400, height: 300)
-
+                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow),annotationItems: place){
+                    MapMarker(coordinate: $0.coordinate,
+                              tint: Color("darkgray"))
+                }
+                .frame(width: 400, height: 300)
+                
+                Button(action: {
+                    openAppleMaps(latitude: self.place[0].coordinate.latitude, longitude: self.place[0].coordinate.longitude)
+                }) {
+                    Text("Get Direction")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(15)
+                        .frame(maxWidth: 120)
+                }
+                .background(Color("darkgray"))
+                .cornerRadius(70)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 0)
+                        .stroke(Color.blue,lineWidth: 0)
+                        .foregroundColor(.black)
+                )
+                
                 Text("")
                 Text("")
                 Text("Contact Details")
@@ -49,7 +78,7 @@ struct Garagedetails: View {
                 HStack {
                     Image(systemName: "envelope")
                         .foregroundColor(.blue)
-                    Text("Email Address:")
+                    Text("Email:")
                         .fontWeight(.semibold)
                         .font(.system(size: 15))
                     Text(detailsview.email)
@@ -62,7 +91,7 @@ struct Garagedetails: View {
                 HStack {
                     Image(systemName: "phone")
                         .foregroundColor(.blue)
-                    Text("Phone Number:")
+                    Text("Phone:")
                         .fontWeight(.semibold)
                         .font(.system(size: 15))
                     Text(detailsview.phone_no)
@@ -72,7 +101,7 @@ struct Garagedetails: View {
                 }
                 .padding(5)
             }
-            Spacer()
+            
             NavigationLink(destination : CustomerDetailsForm(), isActive: $goToCustomerDetailScreen){
                 Button(action : {
                     self.goToCustomerDetailScreen = true
@@ -98,8 +127,45 @@ struct Garagedetails: View {
             
             .navigationBarTitle("", displayMode: .inline)
         }
+        .onAppear(){
+            self.forwardGeocoding(address: detailsview.location)
+        }
         
-        
+    }
+    
+    func forwardGeocoding(address: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address, completionHandler: { (placemarks, error) in
+            if error != nil {
+                print("Failed to retrieve location")
+                return
+            }
+            
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                let coordinate = location.coordinate
+                self.place.append(Place(coordinate: coordinate))
+                print("\(address)")
+                print("\nlat: \(coordinate.latitude), long: \(coordinate.longitude)")
+            }
+            else
+            {
+                print("No Matching Location Found")
+            }
+        })
+    }
+    
+    func openAppleMaps(latitude:CLLocationDegrees,longitude:CLLocationDegrees) {
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = name
+        mapItem.openInMaps(launchOptions: nil)
     }
 }
 
