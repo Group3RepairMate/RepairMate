73fae6709e95fff8f32b35fa045c0aeaf31ed304
harshil3662 <vaghaniharshil11291@gmail.com>
2023-07-01 16:55:11 -0400

    update on mechanic side home screen

--- RepairMate_custLogin/views/Mechanics_Home.swift
+++ RepairMate_custLogin/views/Mechanics_Home.swift
@@ -13,8 +13,28 @@ struct Mechanics_Home: View {
     @State private var orderList: [Order] = []
     
     var body: some View {
-        List(orderList, id: \.id) { order in
-            
+        VStack {
+            if orderList.isEmpty {
+                Text("No orders found")
+                    .foregroundColor(.gray)
+                    .padding()
+            }
+            else{
+                List(orderList, id: \.id) { order in
+                    VStack(alignment: .leading) {
+                        Text("\(order.firstName) \(order.lastName)")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .font(.system(size: 18))
+                        Text("Date : \(order.date)")
+                        Text("location : \(order.location)")
+                    }
+                    .padding()
+                }
+            }
+        }
+        .onAppear {
+            fetchOrderList()
         }
     }
     
@@ -44,8 +64,25 @@ struct Mechanics_Home: View {
                         return
                     }
                     
-                    for order in orders{
+                    if(!orders.isEmpty){
+                        let fetchedOrder = orders.compactMap { order -> Order? in
+                            let data = order.data()
+                            let fname = data["firstName"] as? String ?? ""
+                            let lname = data["lastName"] as? String ?? ""
+                            let contactNo = data["contactNumber"] as? String ?? ""
+                            let email = data["emailAddress"] as? String ?? ""
+                            let problem = data["problemDesc"] as? String ?? ""
+                            let location = data["location"] as? String ?? ""
+                            let garage = data["garagename"] as? String ?? ""
+                            let status = data["status"] as? String ?? ""
+                            let date = data["dateTime"] as? Timestamp ?? Timestamp()
+                            
+                            return Order(firstName: fname, lastName: lname, email: email, date: date.dateValue(), contactNo: contactNo, location: location, garage: garage, status: status, problemDisc: problem)
+                        }
                         
+                        for i in fetchedOrder{
+                            self.orderList.append(i)
+                        }
                     }
                 }
             }
